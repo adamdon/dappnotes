5246c5b70200f6f80694ace66f1b76acbaf36fe4
Adam Don <adamdon89@gmail.com>
2021-05-14 18:49:04 +0100

    Added basic Create from web to database for data Uri from file upload

--- frontend/components/pages/home/HomeInput.js
+++ frontend/components/pages/home/HomeInput.js
@@ -6,6 +6,7 @@ export default {
         return {
             formDisabled: false,
             selectedFile: null,
+            dataUri: "null",
         };
     },
 
@@ -21,17 +22,61 @@ export default {
             window.FilePond.setOptions(
                 {
                     credits: false,
-                    server: 'http://localhost/api/uploadNote'
+                    // server: 'http://localhost/api/uploadNote'
                 });
-            window.FilePond.parse(document.body);
 
+            let inputElement = document.querySelector('input[type="file"]');
+            let filePondInstance = FilePond.create(inputElement);
+            filePondInstance.onpreparefile = function (file){window.dataUri = file.getFileEncodeDataURL()};
 
         },
 
-        onFileChange(event)
+        async uploadNote()
         {
-            console.log(event);
+            console.log("uploadNote");
+
+            if(window.dataUri)
+            {
+                this.dataUri = window.dataUri;
+                this.formDisabled = true;
+
+                let requestBody = {dataUri: this.dataUri,};
+                let requestUrl = "/api/uploadNote";
+                let requestHeaders = {"Content-Type": "application/json"};
+                console.log(requestBody);
+
+                const response = await fetch(requestUrl, {method: "POST", headers: requestHeaders, body: JSON.stringify(requestBody)});
+                const data = await response.json();
+
+                if(data.errors)
+                {
+                    console.log(data.errors[0].message);
+                }
+                else if(data.message)
+                {
+                    console.log("Worked response");
+                    this.dataUri = data.message;
+
+                }
+                else
+                {
+                    console.log("Unexpected response");
+                    console.log(data);
+                }
+            }
+            else
+            {
+                this.dataUri = "upload data first";
+            }
+            this.formDisabled = false;
+
         },
+
+
+        // onFileChange(event)
+        // {
+        //     console.log(event);
+        // },
 
 
     },
@@ -49,13 +94,19 @@ export default {
   <div class="card-header"><i class="fas fa-upload"></i> Input</div>
   
   <div class="card-body">
- 
-    <p class="card-text">
-        text
-    </p>
     
     <label class="form-label" for="customFile">Text Label</label>
-    <input @change="onFileChange" class="form-control filepond" type="file" id="customFile" />
+    <input  class="form-control filepond" type="file" id="customFile" />
+ 
+    
+    <div class="form-group">
+        <button v-on:click="uploadNote" :disabled="formDisabled" type="button" onclick="" class="btn btn-dark w-100">Upload</button>
+    </div> <!-- form-group// -->
+ 
+ 
+     <p class="card-text">
+        {{dataUri}}
+    </p>
     
   </div>
 </div>
