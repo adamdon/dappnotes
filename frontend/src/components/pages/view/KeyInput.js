c008828c3fd1f2c8f84aba139826b4745d7ad118
adamdon <mail@adamdon.co.uk>
2022-03-10 10:53:10 +0000

    Added rinkeby AlchemyProvider to view notes without needing wallet installed.

--- frontend/src/components/pages/view/KeyInput.js
+++ frontend/src/components/pages/view/KeyInput.js
@@ -34,12 +34,169 @@ export default function KeyInput(props)
         }
     }, []);
 
-    async function requestNote()
-    {
 
+    async function requestNoteOnClick()
+    {
+        const currentNetwork = data.config.networkName;
+
+        if(currentNetwork === "localhost")
+        {
+            await requestNoteFromLocalhost();
+        }
+        else if(currentNetwork === "rinkeby")
+        {
+            await requestNoteFromRinkeby();
+        }
+        else
+        {
+            setData({toastError: "Unknown network config: " + currentNetwork})
+        }
+        console.log(currentNetwork);
+
+    }
+
+    async function requestNoteFromRinkeby()
+    {
+        console.log("Requesting note from rinkeby network")
         if(keyInput !== "" && keyInput !== " ")
         {
-            // setComplete(false);
+            setDisabled(true);
+            setData({showSpinner: true});
+
+            try
+            {
+                // let providerOptions = {};
+                // let web3Modal = new Web3Modal({providerOptions});
+                // let instance = await web3Modal.connect();
+                //
+                //
+                // let provider = new ethers.providers.Web3Provider(instance);
+
+                // const provider = new ethers.providers.JsonRpcProvider(data.config.alchemyRinkebyKey)
+                const provider = new ethers.providers.AlchemyProvider("rinkeby", data.config.alchemyRinkebyKey);
+
+
+                // let provider = new ethers.providers.AlchemyProvider("rinkeby", data.config.alchemyRinkebyKey);
+                //
+                // console.log("getting singer");
+                // // let signer = provider.getSigner();
+                // console.log("got singer");
+                // console.log(signer);
+
+
+
+                let deploymentAddress = data.config.deploymentAddress;
+                let contract = new ethers.Contract(deploymentAddress, data.config.contract.abi, provider);
+                console.log("got ethers contract");
+
+                let isContentOwned = await contract.isContentOwned(keyInput);
+                console.log("made is owned call");
+                if(isContentOwned)
+                {
+                    let noteContentString = await contract.getContentByKey(keyInput);
+
+                    if(typeof noteContentString === "string")
+                    {
+                        let noteContentObject;
+
+                        try
+                        {
+                            noteContentObject = JSON.parse(noteContentString);
+                        }
+                        catch (error)
+                        {
+                            setData({toastError: "Error in Note format, " + error.message});
+                        }
+
+                        if(noteContentObject.imageIpfsHash)
+                        {
+
+                            try
+                            {
+                                //requesting cloud data
+                                const requestBody = {imageIpfsHash: noteContentObject.imageIpfsHash};
+                                const requestUrl = (data.backendUrl + "downloadNote/");
+                                const response = await fetch(requestUrl, {method: "POST", headers: {"Content-Type": "application/json"}, body: JSON.stringify(requestBody)});
+                                const jsonData = await response.json();
+                                setCloudImageUri(jsonData.imageUri);
+                            }
+                            catch (error)
+                            {
+                                setData({toastError: "Error could not load cloud data, " + error.message});
+                            }
+
+
+
+                            //note loaded from blockchain
+                            console.log(noteContentObject);
+                            setNoteOutput(noteContentObject);
+                            setKeyInput("");
+                            setComplete(true);
+                            setData({toastSuccess: "Retrieved Note"});
+                        }
+                        else
+                        {
+                            setData({toastError: "Note missing data"});
+                        }
+                    }
+                    else
+                    {
+                        setData({toastError: "Error:  " + noteContentString.toString()});
+                    }
+                }
+                else
+                {
+                    setData({toastError: "Note does not exist"});
+                }
+
+            }
+            catch (error)
+            {
+                if(error.data)
+                {
+                    if(error.data.code && error.data.code === -32603)
+                    {
+                        const contractError = error.data.message.split("reverted with reason string ").pop().slice(1,-1);
+
+                        if(contractError === "Note Already Minted")
+                        {
+                            setData({toastError: contractError});
+                        }
+                        else
+                        {
+                            console.error(contractError);
+                            setData({toastError: error.data.message});
+                        }
+                    }
+                    else
+                    {
+                        console.error(error);
+                        setData({toastError: error.message});
+                    }
+                }
+                else
+                {
+                    console.error(error);
+                    setData({toastError: error.message});
+                }
+
+            }
+
+            setDisabled(false);
+            setData({showSpinner: false});
+        }
+        else
+        {
+            setData({toastMessage: "Input key for note first note:"});
+        }
+
+    }
+
+    async function requestNoteFromLocalhost()
+    {
+        console.log("Requesting note from localhost network")
+        if(keyInput !== "" && keyInput !== " ")
+        {
             setDisabled(true);
             setData({showSpinner: true});
 
@@ -176,8 +333,8 @@ export default function KeyInput(props)
 
                 <div className="input-group mt-3">
                     <span className="input-group-text" >Note Key</span>
-                    <input value={keyInput} onChange={e => setKeyInput(e.target.value)} onKeyDown={(event) => event.key === "Enter" ? requestNote() : {}} disabled={disabled} placeholder="Enter Key" type="text" aria-label="keyInput" id="keyInput"  className="form-control w-50"/>
-                    <button onClick={requestNote} disabled={disabled} className="btn btn-outline-light" type="button" id="submit">Request Note</button>
+                    <input value={keyInput} onChange={e => setKeyInput(e.target.value)} onKeyDown={(event) => event.key === "Enter" ? requestNoteOnClick() : {}} disabled={disabled} placeholder="Enter Key" type="text" aria-label="keyInput" id="keyInput"  className="form-control w-50"/>
+                    <button onClick={requestNoteOnClick} disabled={disabled} className="btn btn-outline-light" type="button" id="submit">Request Note</button>
                 </div>
 
             </div>
